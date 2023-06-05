18ef7c65a75929ef4241120e3b5fb04379f4c215
✪⋆⋇☪ʌɲɲʌɦ δμκз☪⋇⋆✪ <asj@utp.edu.co>
2023-06-05 16:06:43 -0500

    reto 1 platzi resuelto

--- Operacion_matematica_oculta.js
+++ Operacion_matematica_oculta.js
@@ -6,3 +6,33 @@
 18+2=1620 
 21+5=1626
 */
+
+var suma1 =5+4;
+var suma2 = 8+2;
+var suma3 = 10+8;
+var suma4 = 12+9;
+var suma5 = 18+2;
+var suma6 = 21+5;
+
+var resta1 =5-4;
+var resta2 = 8-2;
+var resta3 = 10-8;
+var resta4 = 12-9;
+var resta5 = 18-2;
+var resta6 = 21-5;
+
+var resultado1 = resta1.toString() + suma1.toString ();
+var resultado2 = resta2.toString() + suma2.toString ();
+var resultado3 = resta3.toString() + suma3.toString ();
+var resultado4 = resta4.toString() + suma4.toString ();
+var resultado5 = resta5.toString() + suma5.toString ();
+var resultado6 = resta6.toString() + suma6.toString ();
+
+console.log (resultado1);
+console.log (resultado2);
+console.log (resultado3);
+console.log (resultado4);
+console.log (resultado5);
+console.log (resultado6);
+
+
